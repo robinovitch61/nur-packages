d71ddc755d000aff863ca7139932debd3a276b9e
Leo Robinovitch <leorobinovitch@gmail.com>
2023-11-16 12:28:26 -0800

    goreleaser-testing again

--- default.nix
+++ default.nix
@@ -17,5 +17,6 @@
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
-  wander = pkgs.callPackage ./pkgs/wander { };
+  goreleaser-testing = pkgs.callPackage ./pkgs/goreleaser-testing { };
+  #wander = pkgs.callPackage ./pkgs/wander { };
 }
